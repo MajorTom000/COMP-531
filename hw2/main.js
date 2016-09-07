ed0e398538462f72634a8d327899861784f26c28
wuqiany2 <wuqiany2@msu.edu>
2016-09-06 23:09:12 -0500

    further refined the main.js added more images to the folder

--- hw2/main.js
+++ hw2/main.js
@@ -1,3 +1,4 @@
+//function that returns a random integer between min and max (inclusive)
 function getRandomInt(min, max){
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -5,8 +6,10 @@ function getRandomInt(min, max){
 }
 
 
-
+//object to be assigned to each card div
+//there is a timer going along with it 
 function card(cardDiv, imgFiles){
+    //for refering to the card object when 'this' going into a deeper scope
     var that = this;
 
     var timerID;
@@ -21,13 +24,15 @@ function card(cardDiv, imgFiles){
         timerID = setInterval(function(){
             imgTag[0].setAttribute("src", imgFiles[getRandomInt(1,imgFiles.length)]);
         },this.timerInterval);
+    };
 
+    this.setup = function(){
         var button = cardDiv.getElementsByTagName("BUTTON")[0];
+        if(!button) return;
         button.onclick = function(){
             if (button.innerText == "STOP"){
                 clearInterval(timerID);
                 button.innerText = "START";
-                
             }
             else{
                 that.timerInterval = getRandomInt(1000,5000);
@@ -37,14 +42,14 @@ function card(cardDiv, imgFiles){
         }
     };
 
+    this.setup();
     this.timer();
-
 }
 
 
 window.onload = function(){
     var cards = document.getElementsByClassName("card");
-    var imageFiles = ["1.jpg","2.jpg","3.jpg","4.jpg"];
+    var imageFiles = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg"];
     Array.from(cards).forEach(function(element,value,index){
         element.card = new card(element, imageFiles);
     });
