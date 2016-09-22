f021791df8cbeddeed41a72f4709afeaec025e8e
wuqiany2 <wuqiany2@msu.edu>
2016-09-22 17:46:33 -0500

    refine

--- inclass-10/src/index.js
+++ inclass-10/src/index.js
@@ -28,7 +28,7 @@ class ToDoItem extends React.Component {
         <li id={this.props.id}>
             <i className="check glyphicon glyphicon-check" onClick={()=>{this.setState({done:!this.state.done})}}/>
             <span className={this.state.done?"completed":" "}>{this.props.text}</span>
-            <i className="destroy glyphicon glyphicon-remove" onClick={()=>this.props.list.removeTodo(this.props.id)} />
+            <i className="destroy glyphicon glyphicon-remove" onClick={this.props.remove} />
         </li>
         /*
         h("li", { id: `task${_taskId++}`}, [
@@ -83,7 +83,7 @@ class ToDos extends React.Component {
             <button onClick={this.addTodo}>Add Item</button>
             <span className="submit"><a href="https://webdev-rice.herokuapp.com" target="_blank">Submit your exercise</a></span>
             <ul className="todo">
-                {this.state.todoItems.map((x,i)=><ToDoItem id={x.id} text={x.text} list={this} key={i}/>)}
+                {this.state.todoItems.map((x,i)=><ToDoItem id={x.id} text={x.text} remove={()=>this.removeTodo(x.id)} key={i}/>)}
             </ul>
         </div>
         /*
