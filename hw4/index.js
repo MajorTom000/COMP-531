805a40dfc13666c184ff65bca3de00c46d7a5f31
wuqiany2 <wuqiany2@msu.edu>
2016-10-02 17:16:59 -0500

    bat is drawn

--- hw4/index.js
+++ hw4/index.js
@@ -5,16 +5,22 @@ var createGame = function(canvas){
     var ballx = canvas.width/2 //initial ball position
     var bally = canvas.height/2
     var ballradius = 20
-    var ballvx = 50 //initial ball velocity
-    var ballvy = 5
+    var ballvx = 20 //initial ball velocity
+    var ballvy = 20
 
+    var batx = canvas.width/2
+    var baty = canvas.height - 40
+    var batwidth = 100
+    var batheight = 20
 
 
     var drawBall = function(){
         ballx += ballvx
         bally += ballvy
         if (ballx-ballradius < 0 || ballx+ballradius >canvas.width) ballvx *= -1
-        if (bally-ballradius < 0 || bally+ballradius >canvas.height) ballvy *= -1
+        if (bally-ballradius < 0 ) ballvy *= -1
+
+        if (ballx < batx + batwidth/2 && ballx > batx - batwidth/2 && bally >= baty-batheight && !(bally > baty+batheight)) ballvy *=-1
 
         c.beginPath()
         c.arc(ballx,bally,ballradius,0,Math.PI*2)
@@ -25,15 +31,22 @@ var createGame = function(canvas){
 
 
     var drawBat = function(){
-
+        c.fillStyle = 'black'
+        c.fillRect(batx-batwidth/2, baty-batheight/2, batwidth, batheight);
     }
 
 
     var update = function(){
         c.clearRect(0,0,canvas.width,canvas.height)
         drawBall()
+        drawBat()
     }
 
+	canvas.addEventListener('mousemove',function(e){
+		var x = e.pageX - canvas.offsetLeft;
+		//var y = e.pageY - canvas.offsetTop;
+		batx = x
+	})
 
     return {
         update
